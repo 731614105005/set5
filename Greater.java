410baee9c5dc0b47808fef02a37482941eb71d77
731614105005 <bawadharani96@gmail.com>
2017-07-04 18:31:18 +0530

    Create Greater.java

---
+++ Greater.java
@@ -0,0 +1,25 @@
+# set5
+import java.io.*;
+import java.util.*;
+public class Greater
+{
+  public static void main(String args[])throws IOException
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    if(a>b)
+    {
+      System.out.println("a is greater");
+    }
+    else if(b>c)
+    {
+      System.out.println("b is greater");
+    }
+    else {
+      System.out.println("c is greater");
+    }
+  }
+}
+  
